be1011a3deb93ea9b5451feee55768a4f936ddc8
vgerton <191219k1>
2013-11-12 14:53:55 +0300

    refactoring

--- src/org/htmlparser/client/Answer.java
+++ src/org/htmlparser/client/Answer.java
@@ -9,12 +9,12 @@ public class Answer {
     private String text;
 
     public Answer(String text) {
-        this.text = text;
+        this.text = text.trim();
     }
 
     public Answer(String tagsString, String text) {
         this.innerTagsString = tagsString;
-        this.text = text;
+        this.text = text.trim();
     }
 
     public void print() {
@@ -23,6 +23,8 @@ public class Answer {
                 System.out.println(Constants.INNER_TAGS_ARE_NOT_FOUND);
             } else
                 System.out.println("Вложенные теги: " + innerTagsString);
+        if (text.length()==0)
+            text=Constants.EMPTY_TAG_VALUE;
         System.out.println(text);
     }
 
