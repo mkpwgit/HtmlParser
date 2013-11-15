ff3bb709a0f8f4dad474e019456528378440887a
vgerton <191219k1>
2013-11-15 19:44:52 +0300

    refactoring

--- src/org/htmlparser/fileprocessing/OpeningTagState.java
+++ src/org/htmlparser/fileprocessing/OpeningTagState.java
@@ -11,12 +11,6 @@ public class OpeningTagState implements State {
     private HtmlReader htmlReader;
     private StringBuilder tag;
 
-    public OpeningTagState(HtmlReader htmlReader, Tree tree) {
-        this.htmlReader = htmlReader;
-        this.tree = tree;
-        this.tag = new StringBuilder();
-    }
-
     public OpeningTagState(HtmlReader htmlReader, char c, Tree tree) {
         this.htmlReader = htmlReader;
         this.tag = new StringBuilder().append(c);
